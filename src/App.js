cad2729e12fae29a760be443783bd405c46b543c
Kae Benton <sonja.k.benton@gmail.com>
2021-03-19 18:27:09 -0400

    ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { fetchMovies } from './store/actions';
 import MovieList from './components/MovieList';
